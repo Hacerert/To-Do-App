59ead4b2c05af9f9f8f97ebb0535e719aaa9264c
Hacer Ertuğrul <hacerertugrulq@gmail.com>
2026-01-25 12:49:10 +0300

    Boş görev engeli ve silme özelliği eklendi

--- script.js
+++ script.js
@@ -3,9 +3,20 @@ const button=document.getElementById("btn");
 const liste=document.getElementById("list");
 
 button.addEventListener("click",function(){
-const text=input.value;
+const text=input.value.trim();//trim boşluk saymasın demek
+if(text===""){
+    return;
+}
 const li=document.createElement("li");
-li.textContent=text;
+const span=document.createElement("span");
+const deleteBtn=document.createElement("button");
+span.textContent=text;
+deleteBtn.textContent="Sil"
+deleteBtn.addEventListener("click",function(){
+li.remove();
+});
+li.appendChild(span);
+li.appendChild(deleteBtn);
 liste.appendChild(li);
 input.value="";
 });
