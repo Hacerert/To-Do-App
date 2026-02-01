a495f181e4b69379bdfe7a21c152632662fa3d82
Hacer Ertuğrul <hacerertugrulq@gmail.com>
2026-02-01 14:11:08 +0300

    enter tuşuna basınca ekleme özelliği eklendi

--- script.js
+++ script.js
@@ -1,22 +1,39 @@
+let tasks = JSON.parse(localStorage.getItem("tasks"))|| [];//veriyi başlatıyoruız
 const input=document.getElementById("txt");
-const button=document.getElementById("btn");
+const button=document.getElementById("btn");//html ile tanış
 const liste=document.getElementById("list");
 
-button.addEventListener("click",function(){
-const text=input.value.trim();//trim boşluk saymasın demek
-if(text===""){
-    return;
+function ekranaBas(){//ekrana bas fonsk
+    liste.innerHTML="";//ekranı temizle
+    tasks.forEach(function(gorev,index){//dizi dolaş
+        const li=document.createElement("li");//görev için satır
+        const span=document.createElement("span");//yazıyı tutacak span
+        span.textContent=gorev;
+        const deleteBtn=document.createElement("button");//sil butonu
+        deleteBtn.textContent="Sil";
+        deleteBtn.addEventListener("click",function(){//silme işi
+            tasks.splice(index,1);//sildi
+             localStorage.setItem("tasks", JSON.stringify(tasks));//güncelle
+             ekranaBas();//ekrana bir daha yaz
+    });
+    li.appendChild(span);
+        li.appendChild(deleteBtn);//elemanları birleştir
+        liste.appendChild(li);
+        });
 }
-const li=document.createElement("li");
-const span=document.createElement("span");
-const deleteBtn=document.createElement("button");
-span.textContent=text;
-deleteBtn.textContent="Sil"
-deleteBtn.addEventListener("click",function(){
-li.remove();
+button.addEventListener("click",function(){//ekle butonu
+    const text=input.value.trim();
+    if(text===""){//boş mu kontrol
+        return;
+    }
+    tasks.push(text);//diziye ekle
+     localStorage.setItem("tasks", JSON.stringify(tasks));//depoya yaz
+     ekranaBas();//yeniden bas
+     input.value="";//input temizle
 });
-li.appendChild(span);
-li.appendChild(deleteBtn);
-liste.appendChild(li);
-input.value="";
+input.addEventListener("keydown",function(event){
+if(event.key==="Enter"){//entera basınca ekliyor
+    button.click();
+}
 });
+ekranaBas();//sayfa açılııncaa
